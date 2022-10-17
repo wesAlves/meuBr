d801cd7deeab6bac08fa3fa6a4e9dd5fb9149bc9
wes <wal2.designer@gmail.com>
2022-10-17 21:04:29 +0100

    migrate news and events

--- news_and_events/models.py
+++ news_and_events/models.py
@@ -4,16 +4,20 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
-class News_and_events(models.Model):
+class Post(models.Model):
     slug = models.SlugField(default='', null=False)
     title = models.CharField(max_length=100)
     source = models.CharField(max_length=100)
-    excerpt = models.CharField(max_length=300)
+    description = models.CharField(max_length=300)
     content = models.CharField(max_length=1000)
     type = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
     authors = models.CharField(max_length=100)
     status = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    photos = models.CharField(max_length=50)
+    event_starts = models.CharField(max_length=10)
+    event_ends = models.CharField(max_length=10)
     created_at = models.CharField(max_length=10)
     updated_at = models.CharField(max_length=10)
 
