bfdc599eed0282b3d68189908b2247f70a365b5d
wes <wal2.designer@gmail.com>
2022-10-17 20:57:31 +0100

    news and events model

--- news_and_events/models.py
+++ news_and_events/models.py
@@ -1,3 +1,23 @@
+from email.policy import default
+from unicodedata import category
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class News_and_events(models.Model):
+    slug = models.SlugField(default='', null=False)
+    title = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300)
+    content = models.CharField(max_length=1000)
+    type = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    authors = models.CharField(max_length=100)
+    status = models.CharField(max_length=50)
+    created_at = models.CharField(max_length=10)
+    updated_at = models.CharField(max_length=10)
+
+    def save(self, *args, **kwargs ):
+        self.slug = slugify(self.title)
+        super().save()
+
